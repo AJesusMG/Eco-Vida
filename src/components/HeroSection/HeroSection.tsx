accbf4bd444e5bc8e85d2796bb3092a267b16c71
ElYizuz29 <amorales130@alumnos.uaq.mx>
2024-06-21 00:28:25 -0600

    Added a few changes

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -2,7 +2,7 @@ import { Image } from "@nextui-org/react";
 
 export default function HeroSection() {
   return (
-    <div className="h-screen flex flex-col md:flex-row justify-between items-center px-8 gap-4">
+    <div className="h-screen flex flex-col md:flex-row justify-between items-center px-4 md:px-8 gap-4 md:gap-8">
       <div className="flex flex-col gap-4 w-full md:w-1/2 text-center md:text-left">
         <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold">
           ¿Neta qué les <br /> cuesta?
@@ -15,12 +15,12 @@ export default function HeroSection() {
           Si no tambien a la prevencion sobre el uso de sustancias psicoactivas tomando encuestas, videos y recomendaciones.
         </p>
       </div>
-      <div id="image" className="w-full md:w-3/6 flex justify-center md:justify-end">
+      <div id="image" className="w-full md:w-1/2 flex justify-center md:justify-end mt-4 md:mt-0">
         <Image
           src="/HeroImage.jpg"
           alt="Picture of the author"
           radius="sm"
-          width={800}
+
         />
       </div>
     </div>
